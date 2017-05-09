bebb511e023795364a8c03ade19e2e93421bba58
dwarikas <dwarika.silwal@gmail.com>
2017-05-09 17:37:40 -0400

    Add files via upload

---
+++ CustomerPortal/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+
+
+namespace CustomerPortal.Controllers
+{
+    public class BaseController : ApiController {
+       
+    }
+}
---
+++ CustomerPortal/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using Portal.Web;
+using System;
+
+namespace CustomerPortal.Controllers
+{
+    public class CustomerController : BaseController {
+
+        [Route("api/customers")]
+        [HttpGet]
+        public System.Collections.Generic.IEnumerable<CustomerData> GetLocations() {
+            try {
+                return App.Instance.GetLocations();
+            } catch (Exception ex) {
+                throw  new Exception ("CustomerController.GetCustomer", ex);
+                
+            }
+        }
+
+
+        [Route("api/customer")]
+        [HttpPost]
+        public void AddCustomer([FromBody]CustomerData cd) {
+            try {
+                App.Instance.AddCustomer(cd);
+            } catch (Exception ex) {
+                throw new Exception("CustomerController.AddCustomer", ex);
+            }
+        }
+
+        [Route("api/cusEdit")]
+        [HttpPost]
+        public void EditCustomer([FromBody]CustomerData cd) {
+            try {
+                App.Instance.EditCustomer(cd);
+            } catch (Exception ex) {
+                throw new Exception("CustomerController.EditCustomer", ex);
+            }
+        }
+    
+        [Route("api/customer/{id}")]
+        [HttpDelete]
+        public void DeleteCustomer(int id) {
+            try {
+                App.Instance.DeleteCustomer(id);
+            } catch (Exception ex) {
+                throw new Exception("CustomerController.DeleteCustomer", ex);
+            }
+        }
+
+    }
+}
